8483060fd83f889193efca556acf4ff03f5c1059
kevincayenne <kevin81319@gmail.com>
2016-11-01 08:15:23 +0000

    first commit

---
+++ helloworldmethod.rb
@@ -0,0 +1,8 @@
+def hello(anything)
+  puts anything
+end
+
+hello "My name is Kevin!"
+hello "Welcome to the Complete Ruby on Rails Developer Course!"
+
+
---
+++ playgroound.rb
@@ -0,0 +1,7 @@
+puts "Hello enter your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+l_name = gets.chop
+
+puts "Whlcome #{first_name} #{l_name}, to the playground!"
