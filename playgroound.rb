86f5b446bab429042476c366e26911821ce1ca43
kevin <kevin81319@gmail.com>
2016-11-02 17:50:03 +0800

    second commit

--- playgroound.rb
+++ playgroound.rb
@@ -4,11 +4,8 @@ first_name = gets.chomp
 puts "What is your last name?"
 l_name = gets.chop
 
-<<<<<<< HEAD
 puts "Whlcome #{first_name} #{l_name}, to the playground!"
 
 puts "your first name has #{first_name.length} characters."
 puts "your last name has #{l_name.length} characters."
-=======
-puts "Whlcome #{first_name} #{l_name}, to the playground!"
->>>>>>> 8483060fd83f889193efca556acf4ff03f5c1059
+
