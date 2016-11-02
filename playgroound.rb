9f1abb9108b4833b8b1eb18dbd84a2721e0e8bfe
kevin <kevin81319@gmail.com>
2016-11-02 17:39:04 +0800

    second edited

---
+++ playgroound.rb
@@ -0,0 +1,10 @@
+puts "Hello enter your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+l_name = gets.chop
+
+puts "Whlcome #{first_name} #{l_name}, to the playground!"
+
+puts "your first name has #{first_name.length} characters."
+puts "your last name has #{l_name.length} characters."
