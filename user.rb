f51d0d4ecfcb392cafaec8eb54301b367bb1640c
kevin <kevin81319@gmail.com>
2016-11-06 15:20:03 +0800

    user.rb final-1 update

--- user.rb
+++ user.rb
@@ -1,17 +1,39 @@
 class User
 
-
+	attr_accessor :name
 	def initialize(name)
 		@name = name
 	end 
+
+	def run 
+		p "Hey I'm runnung!"
+	end
+
+	# def get_name
+	# 	@name
+	# end
+
+	# def set_name=(name) 
+	# 	@name = name
+	# end
+
 end
 
 user = User.new("John")
-user1 = User.new("Kevin")
-user2 = User.new("Hacker")
+p user.name
+user.name = "Kevin"
+p user.name
+
+# user1 = User.new("Kevin")
+# user2 = User.new("Hacker")
 
 # p User.ancestors
 
-p user
-p user1
-p user2
+# user.run
+# user1.run
+# user2.run
+
+# p user
+# p user1
+# p user2
+
